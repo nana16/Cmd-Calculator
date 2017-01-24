0c3d98f08f03b4af0b5efa6f75f498b6a8004217
nana16 <notengdarkwah@gmail.com>
2017-01-24 13:40:29 +0000

    add function implemented

---
+++ first.java
@@ -0,0 +1,13 @@
+ public class Px{
+	 
+	 public static void main (String [] parameters){
+		 
+		 if (parameters[0].equals("add"){
+			 double one = Double.parseDouble (parameters[1]);
+			 double two = Double.parseDouble (parameters[2]);
+			 System.out.println(one+two);
+		 }
+	 }	 
+ }
+ 
+ 
