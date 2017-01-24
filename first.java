6b9920db07d5852cc94509763835a0d68adb5d2f
nana16 <notengdarkwah@gmail.com>
2017-01-24 13:58:50 +0000

    multiply function implemented

--- first.java
+++ first.java
@@ -8,10 +8,10 @@
 			 System.out.println(one+two);
 		 }
 		 
-		 if (parameters[0].equals("subtract"){
+		 if (parameters[0].equals("multiply"){
 			 double one = Double.parseDouble (parameters[1]);
 			 double two = Double.parseDouble (parameters[2]);
-			 System.out.println(one-two);
+			 System.out.println(one*two);
 		}
 	 }	 
  }
