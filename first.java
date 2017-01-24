19072424cbb0a95442bc5667bbba52ca8f8f833b
nana16 <notengdarkwah@gmail.com>
2017-01-24 13:49:23 +0000

    subtract function implemented

--- first.java
+++ first.java
@@ -7,6 +7,12 @@
 			 double two = Double.parseDouble (parameters[2]);
 			 System.out.println(one+two);
 		 }
+		 
+		 if (parameters[0].equals("subtract"){
+			 double one = Double.parseDouble (parameters[1]);
+			 double two = Double.parseDouble (parameters[2]);
+			 System.out.println(one-two);
+		}
 	 }	 
  }
  
