8f8b3675b8c57341c7b3eff3781a2e4cec9d4602
Mills <odarkwei.mills@ashesi.edu.gh>
2017-01-25 00:01:18 +0000

    divide

--- first.java
+++ first.java
@@ -20,6 +20,12 @@
 			 double two = Double.parseDouble (parameters[2]);
 			 System.out.println(one-two);
 		 }
+		 
+		 if (parameters[0].equals("divide"){
+			 double one = Double.parseDouble (parameters[1]);
+			 double two = Double.parseDouble (parameters[2]);
+			 System.out.println(one/two);
+		 }
 
 	 }	 
  }
